a2a062d33e7bd982dd2d1dd110e372df462c8d81
Jeff Conaway <liftorgohome@gmail.com>
2020-09-13 17:27:22 -0400

    changes to support figlet function

--- locustfile.py
+++ locustfile.py
@@ -1,38 +1,13 @@
-import lorem
-from locust import HttpUser, task, between, SequentialTaskSet
+import time
+from locust import HttpUser, task, between
 
 
-class TodoUser(HttpUser):
-    wait_time = between(2, 5)
-    todo_item = None
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2)
 
     @task
-    class CrudSequence(SequentialTaskSet):
-        @task
-        def create_todo(self):
-            headers = {"content-type": "application/json"}
-            response = self.client.post(
-                "/todo",
-                json={"Description": lorem.sentence(), "Completed": False},
-                headers=headers,
-            )
-            self.user.todo_item = response.json()
-
-        @task
-        def retrieve_todo(self):
-            self.client.get(f"/todo/{self.user.todo_item['Id']}", name="/todo/[id]")
-
-        @task
-        def update_todo(self):
-            headers = {"content-type": "application/json"}
-            self.user.todo_item["Completed"] = True
-            self.client.put(
-                f"/todo/{self.user.todo_item['Id']}",
-                json=self.user.todo_item,
-                headers=headers,
-                name="/todo/[id]"
-            )
-
-        @task
-        def delete_todo(self):
-            self.client.delete(f"/todo/{self.user.todo_item['Id']}", name="/todo/[id]")
+    def index_page(self):
+        self.client.post(
+            "/function/figlet", "Software is a child that never goes to sleep. Always needing something.", name="/software")
+        self.client.get(
+            "/function/figlet", "Young women going through the hippie stage of putting their mattress on the floor.", name="/women")
